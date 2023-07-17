209a6028c8645956be3bb04e513969fa0db8c111
smhrd <smhrd@218.157.38.54>
2023-07-17 16:00:32 +0900

    commit

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -7,10 +7,6 @@ public class Ex01 {
 		System.out.println("첫번째 커밋");
 		System.out.println("팀원 1 :두번째 커밋");
 		System.out.println("팀원 2 :세번째 커밋");
-<<<<<<< HEAD
-=======
-		System.out.println("영준 - 하하호호");
->>>>>>> branch 'master' of https://github.com/2023-SMHRD-IS-BigData1/CHTeam.git
 
 	}
 
